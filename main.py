a474e8c8e4d7d9cbf6eb686ed92cdfbb60a13aab
Audun Ljone Henriksen <audun.henriksen@gmail.com>
2021-01-04 20:38:25 +0100

    I added all the imports I assume I will be using. Thank you for help Ody

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 if __name__ == "__main__":
     pass
