73d03400128d58891a109a5b6f8b3d2f93294241
Odin <odin@severinsen.no>
2021-01-07 21:23:20 +0100

    commit before merge

--- ml/visualization.py
+++ ml/visualization.py
@@ -1,6 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
+import graphics as g
+
+
+class GUI:
+    def __init__(self, size):
+        self.colors = ['red1', 'blue1', 'blue2', 'blue3', 'blue4', 'DeepSkyBlue', 
+                        'DeepSkyBlue1', 
+                        'DeepSkyBlue2', 
+                        'DeepSkyBlue3', 
+                        'DeepSkyBlue4']
+        self.size = size
+        self.win = g.GraphWin(height = 800, width = 800) # create a window
+        self.win.setCoords(0, 0, size, size)
+        self.squares = []
+        self.numbers = []
+        for i in range(size):
+            self.squares.append([])
+            self.numbers.append([])
+            for j in range(size):
+                mySquare = g.Rectangle(g.Point(i, j),
+                                          g.Point(i+1, j+1))
+                myNum = g.Text(g.Point(i+0.5, j+0.5), "")
+                mySquare.draw(self.win)
+                myNum.draw(self.win)
+                self.squares[i].append(mySquare)
+                self.numbers[i].append(myNum)
+        
+    def loadMap(self, board1, board2, percents, coords):
+        for i in range(2, self.size+2):
+            for j in range(2, self.size+2):
+                if board2[i][j] == False:
+                    self.squares[i-2][j-2].setFill(self.colors[int(board1[i, j])+1])
+                    self.numbers[i-2][j-2].setText(str(int(board1[i, j])))
+
+                else:
+                    self.squares[i-2][j-2].setFill("Green")
+                    self.numbers[i-2][j-2].setText(str(round(percents[coords.index((i, j))], 2)) + "%")
+                    
+                
+    def loadColor(self, x, y, color):
+        self.squares[x-2][y-2].setFill(color)
+       
+    def close(self):
+        self.win.close()
+
 
 class Visualizer:
     def __init__(self, w=10, h=10, init_probs=np.random.rand(10, 10)):
