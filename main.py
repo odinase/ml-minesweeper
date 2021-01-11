4a76bafaf3f32ce0ab4dd0e2ffcf02590af580b9
Audun Ljone Henriksen <audun.henriksen@gmail.com>
2021-01-11 14:11:25 +0100

    Removed wallsize, made a pipiline #pipeline

--- main.py
+++ main.py
@@ -19,35 +19,44 @@ import time
 # profile = line_profiler.LineProfiler()
 # atexit.register(profile.print_stats)
 
-MAX_DATA_POINTS = 1000
+MAX_DATA_POINTS = 100000
 NUM_BOMBS = 15
 SIZE = 10
+ONE_HOT_ENCODING = True
+DIM = 9 if ONE_HOT_ENCODING else 1
 
 def create_datapoint_python(game):
     # Makes a copy, just in case, to not cause shenanigans outside of function
-    b = game.board(with_walls=False)
-    categories = np.arange(9).reshape(-1,1) # 0:8 is the value of the tile, 9 is covered. Bomb tiles not necessary due to redundant
-
+    b = game.board()
+    
     labels = (b == -1).reshape(-1,1).astype(int)
     # print(labels.ravel())
     # input()
-    features = (categories == b.ravel()).T.astype(int)
-
-    data_point = np.hstack((features.ravel(), labels.ravel()))
-
+    if ONE_HOT_ENCODING:
+        categories = np.arange(9).reshape(-1,1) # 0:8 is the value of the tile, 9 is covered. Bomb tiles not necessary due to redundant
+        features = (categories == b.ravel()).T.astype(int)
+        data_point = np.hstack((features.ravel(), labels.ravel()))
+    else:
+        data_point = np.hstack((b.ravel(), labels.ravel()))
     return data_point
 
 def get_path(files):
+    if ONE_HOT_ENCODING:
+        ending = '_one_hot'
+    else:
+        ending = '_no_hot'
     return str('./data/dataPoints' + str(MAX_DATA_POINTS)+'_'+str(NUM_BOMBS)+
-            '_'+str(SIZE)+'_num='+str(files)+'.csv')
+            '_'+str(SIZE)+'_num='+str(files)+ending+'.csv')
 
 def generate_features_predict_python(game):
     # Makes a copy, just in case, to not cause shenanigans outside of function
-    b = game.board(with_walls=False)
-    categories = np.arange(9).reshape(-1,1) # 0:8 is the value of the tile, 9 is covered. Bomb tiles not necessary due to redundant
-
-    features = (categories == b.ravel()).T.astype(int).flatten()
-
+    b = game.board()
+    
+    if ONE_HOT_ENCODING:
+        categories = np.arange(9).reshape(-1,1) # 0:8 is the value of the tile, 9 is covered. Bomb tiles not necessary due to redundant
+        features = (categories == b.ravel()).T.astype(int).flatten()
+    else:
+        features = b.flatten()
     return features
 
 # @profile
@@ -60,17 +69,18 @@ def generate_data_point(game, files):
     
     while(curr_num_data_points < MAX_DATA_POINTS):
         count+= 1
-        decimal = curr_num_data_points/MAX_DATA_POINTS
-        if count < decimal:
-            count = int(np.ceil(decimal))
-            print("We're {0}% of the way with file {1} out of {2}".format(decimal*100, files_made+1, files_made+files_left))
+        percent = 100*(curr_num_data_points/MAX_DATA_POINTS)
+        if count < percent:
+            count = int(np.ceil(percent))
+            print("We're {0}% of the way with file {1} out of {2}".format(percent, files_made+1, files_made+files_left))
         
         game.rand_click(SIZE)
-        dataPoints.append(create_datapoint_python(game))
         
-        curr_num_data_points += 1
-        game.reset()
-        
+        if game.get_state() == Board.State.GameOver:
+            game.reset()
+        else:
+            dataPoints.append(create_datapoint_python(game))
+            curr_num_data_points += 1
     dataPoints = np.vstack(dataPoints)
 
     start = time.time()
@@ -82,12 +92,20 @@ def generate_data_point(game, files):
 
 def get_model():
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-    input_shape = (SIZE, 9, 1)
+    input_shape = (SIZE, SIZE, DIM)
     model = Sequential()
-    model.add(Conv2D(SIZE*SIZE, (3, 3), input_shape = input_shape, activation='relu'))
+    model.add(Conv2D(2, 4, input_shape = input_shape, activation='relu', padding='SAME'))
     model.add(Flatten())
-    model.add(Dense(SIZE*SIZE))
+    model.add(Dense(SIZE*SIZE, activation='relu'))
     model.add(Softmax())
+    # model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape = input_shape))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
+    # model.add(Flatten())
+    # model.add(Dense(SIZE*SIZE))
+    # model.add(Softmax())
 
     return model
 
@@ -98,6 +116,76 @@ def fit_model(model, X_train, Y_train, epochs = 1):
               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=['BinaryAccuracy'])
     model.fit(X_train, Y_train, epochs=epochs, use_multiprocessing=True, workers = 240)
+    
+
+def test_with_graphics(game):
+    game.reset() 
+    game.rand_click(SIZE)
+    graphics = GUI(size=SIZE)
+    while(True):
+        probs = []
+
+        dataPoint = generate_features_predict_python(game)
+
+        probs = model.predict_on_batch(dataPoint.reshape(1, -1).reshape(1, SIZE, SIZE, DIM))
+        
+        probs = (probs[0]*100).reshape(SIZE, SIZE)
+        np.set_printoptions(linewidth=180, precision=2, suppress=True)
+        
+        for x, y in np.vstack(np.unravel_index(probs.ravel().argsort(), probs.shape)).T:
+            if game._covered_board[x,y]:
+                i = x
+                j = y
+                break
+                
+        graphics.loadMap(game._board, game._covered_board, probs)
+        graphics.loadColor(i, j, 'yellow')
+        graphics.win.getMouse()
+
+        game.tile_click((i, j))
+        print("\n There are {} more squares you need to uncover!".format(game.tiles_left()))
+        if game.get_state() != Board.State.Playing:
+            if game.get_state() == Board.State.GameOver:
+                print("\nYou lost... RIP")
+            else:
+                print("\nWow, you actually won!")
+            graphics.loadMap(game._board, game._covered_board, probs)
+            graphics.win.getMouse()
+            game.reset() 
+            game.rand_click(SIZE)
+    
+def pipeline(game):
+    game.reset() 
+    game.rand_click(SIZE)
+    score = 0
+    for i in range(1000):
+        if i % 100 == 0:
+            print(i)
+        temp_score = 0
+        while(True):
+            probs = []
+    
+            dataPoint = generate_features_predict_python(game)
+    
+            probs = model.predict_on_batch(dataPoint.reshape(1, -1).reshape(1, SIZE, SIZE, DIM))
+            
+            probs = (probs[0]*100).reshape(SIZE, SIZE)
+            
+            for x, y in np.vstack(np.unravel_index(probs.ravel().argsort(), probs.shape)).T:
+                if game._covered_board[x,y]:
+                    i = x
+                    j = y
+                    break
+            game.tile_click((i, j))
+            
+            if game.get_state() != Board.State.Playing:
+                game.reset() 
+                game.rand_click(SIZE)
+                break
+            else:
+                temp_score += 1
+        score += temp_score
+    print(score/1000)
     
     
 if __name__ == "__main__":
@@ -118,57 +206,26 @@ if __name__ == "__main__":
     end = time.time()
     print(f"took {end-start} s")
     
-    
     print("\nStarting training....")
     model = get_model()
     model.summary()
-    input()
     for files in range(num_of_files):
         dataPoints = pd.read_csv(get_path(files), compression = 'gzip').to_numpy()
 
         X_train = dataPoints[:,1:-(SIZE*SIZE)]
         Y_train = dataPoints[:,-(SIZE*SIZE):]
-        X_train = X_train.reshape(MAX_DATA_POINTS, SIZE*SIZE, 9, 1)
+        
+        X_train = X_train.reshape(MAX_DATA_POINTS, SIZE, SIZE, DIM)
+        
+        Y_train.reshape(MAX_DATA_POINTS, SIZE, SIZE, 1)
         
         fit_model(model, X_train, Y_train)
 
     print("\nTraining complete. I am at your service, Master!")
-    game.reset() 
-    game.rand_click(SIZE)
-    graphics = GUI(size=SIZE)
+    pipeline(game)
     
-    
-    while(True):
-        probs = []
 
-        dataPoint = generate_features_predict_python(game)
 
-        probs = model.predict_on_batch(dataPoint.reshape(1, -1))
-        
-        probs = (probs[0]*100).reshape(SIZE, SIZE)
-        np.set_printoptions(linewidth=180, precision=2, suppress=True)
-        
-        for x, y in np.vstack(np.unravel_index(probs.ravel().argsort(), probs.shape)).T:
-            if game._covered_board[x,y]:
-                i = x
-                j = y
-                break
-                
-        graphics.loadMap(game._board, game._covered_board, probs)
-        graphics.loadColor(i, j, 'yellow')
-        graphics.win.getMouse()
-
-        tile = game.tile_click((i, j))
-        print("\n There are {} more squares you need to uncover!".format(game.tiles_left()))
-        if game.get_state() != Board.State.Playing:
-            if game.get_state() == Board.State.GameOver:
-                print("\nYou lost... RIP")
-            else:
-                print("\nWow, you actually won!")
-            graphics.loadMap(game._board, game._covered_board, probs)
-            graphics.win.getMouse()
-            game.reset() 
-            game.rand_click(SIZE)
 
 
 
--- minesweeper/board.py
+++ minesweeper/board.py
@@ -21,13 +21,11 @@ class Board:
         GameOver = 1
         Won = 2
     
-    def __init__(self, grid_size=(10, 10), num_bombs=10, wallSize = 0):
-        self.wallSize = wallSize
+    def __init__(self, grid_size=(10, 10), num_bombs=10):
         self.grid_size = grid_size
         self.num_bombs = num_bombs
         self.w, self.h = grid_size
-        self._covered_board = np.full((self.w+2*wallSize, self.h+2*wallSize), False)
-        self._covered_board[wallSize:(self.w+wallSize), wallSize:(self.h+wallSize)] = True
+        self._covered_board = np.full((self.w, self.h), True)
         self._board = None
         self._state = Board.State.Playing
         self.first_clicked = False
@@ -38,33 +36,22 @@ class Board:
     def set_state(self, state):
         self._state = state
 
-    def board(self, with_walls=True):
-        b = None
-        if with_walls:
-            b = self._board
-        else:
-            b = self._board[self.wallSize:(self.grid_size[0]+self.wallSize), self.wallSize:(self.grid_size[1]+self.wallSize)]
-        return b
+    def board(self):
+        return self._board
 
-    def covered_board(self, with_walls=True):
-        cb = None
-        if with_walls:
-            cb = self._covered_board
-        else:
-            cb = self._covered_board[self.wallSize:(self.grid_size[0]+self.wallSize), self.wallSize:(self.grid_size[1]+self.wallSize)]
-        return cb
+    def covered_board(self):
+        return self._covered_board
         
     def reset(self):
-        self._covered_board = np.full((self.w+self.wallSize*2, self.h+self.wallSize*2), False)
-        self._covered_board[self.wallSize:(self.w+self.wallSize), self.wallSize:(self.h+self.wallSize)] = True
+        self._covered_board = np.full((self.w, self.h), True)
         self._board = None
         self._state = Board.State.Playing
         self.first_clicked = False
         
     def rand_click(self, SIZE):
-        a = np.random.randint(self.wallSize, SIZE+self.wallSize)
-        b = np.random.randint(self.wallSize, SIZE+self.wallSize)
-        self.tile_click((a, b))
+        coords = np.argwhere(self._covered_board == True)
+        index = np.random.randint(coords.shape[0])
+        self.tile_click(tuple(coords[index]))
         
         
     def __make_board(self, coord):
@@ -73,12 +60,12 @@ class Board:
         self._board.ravel()[:self.num_bombs] = -1
         np.random.shuffle(self._board.ravel())
         
-        if self._board[coord[0] - self.wallSize, coord[1] - self.wallSize] == -1:
+        if self._board[coord[0], coord[1]] == -1:
             not_bomb_grid = self._board == 0
             r_idx, c_idx = not_bomb_grid.nonzero() # corresponding row and col idxs of bomb-free tiles
             other_tile = np.random.randint(0, r_idx.shape[0])
             self._board[r_idx[other_tile], c_idx[other_tile]] = -1
-            self._board[coord[0] - self.wallSize, coord[1] - self.wallSize] = 0
+            self._board[coord[0], coord[1]] = 0
         for i in range(self.w):
             for j in range(self.h):
                 if self._board[i,j] == -1:
@@ -86,14 +73,6 @@ class Board:
                 neighs = self.neighbours((i, j))
                 num_bombs = sum([1 for n in neighs if n == -1])
                 self._board[i, j] = num_bombs
-        
-        for i in range(self.wallSize):
-            self._board = np.vstack((
-                np.full(self.w + 2*i, -2),
-                self._board,
-                np.full(self.w + 2*i, -2),
-            ))
-            self._board = np.hstack((np.full((self.h+2*(i+1),1), -2), self._board, np.full((self.h+2*(i+1),1), -2)))
         return self._board
 
     def tile_click(self, coord):
@@ -114,8 +93,8 @@ class Board:
         if self._board[coord] > 0:
             return tile
         
-        for i in range(max(x-1, self.wallSize), min(x+2, self.w+self.wallSize)):
-            for j in range(max(y-1, self.wallSize), min(y+2, self.h+self.wallSize)):
+        for i in range(max(x-1, 0), min(x+2, self.w)):
+            for j in range(max(y-1, 0), min(y+2, self.h)):
                 if self._board[i, j] >= 0 and self._covered_board[i, j] == True:
                     self.tile_click((i, j))
                     
@@ -141,9 +120,9 @@ class Board:
         for j in range(0, 4*self.h+7):
             print("-", end='')
         print()
-        for i in range(self.wallSize, self.w+self.wallSize):
+        for i in range(self.w):
             print(f'{i}: |', end='')
-            for j in range(self.wallSize, self.h+self.wallSize):
+            for j in range(self.h):
                 t = int(self._board[i,j])
                 if self._covered_board[i,j]:
                     t = "x"
